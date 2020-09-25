fb036656666e658055d3527b20be061d4e4dfb3c
marym <maria_manzhos@tut.by>
2020-09-25 16:54:32 +0300

    changed locators (from XPath to CssSelector)

--- src/test/java/HoversTest.java
+++ src/test/java/HoversTest.java
@@ -4,23 +4,21 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import javax.swing.*;
-
 public class HoversTest extends SetUp{
 
     @Test
     public void hover1Test(){
 
         driver.get("http://the-internet.herokuapp.com/hovers");
-        WebElement img = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/img"));
+        WebElement img = driver.findElement(By.cssSelector("#content > div > div:nth-child(3) > img"));
         Actions action = new Actions(driver);
         action.moveToElement(img).perform();
 
-        WebElement user = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/h5"));
+        WebElement user = driver.findElement(By.cssSelector("#content > div > div:nth-child(3) > div > h5"));
         String actualResult1 = user.getText();
         Assert.assertEquals(actualResult1, "name: user1");
 
-        WebElement link = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/a"));
+        WebElement link = driver.findElement(By.linkText("View profile"));
         action.moveToElement(link).click().perform();
         WebElement name = driver.findElement(By.tagName("h1"));
         String actualResult2 = name.getText();
@@ -32,15 +30,15 @@ public class HoversTest extends SetUp{
     public void hover2Test(){
 
         driver.get("http://the-internet.herokuapp.com/hovers");
-        WebElement img = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/img"));
+        WebElement img = driver.findElement(By.cssSelector("#content > div > div:nth-child(4) > img"));
         Actions action = new Actions(driver);
         action.moveToElement(img).perform();
 
-        WebElement user = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/h5"));
+        WebElement user = driver.findElement(By.cssSelector("#content > div > div:nth-child(4) > div > h5"));
         String actualResult1 = user.getText();
         Assert.assertEquals(actualResult1, "name: user2");
 
-        WebElement link = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/a"));
+        WebElement link = driver.findElement(By.linkText("View profile"));
         action.moveToElement(link).click().perform();
         WebElement name = driver.findElement(By.tagName("h1"));
         String actualResult2 = name.getText();
@@ -52,15 +50,15 @@ public class HoversTest extends SetUp{
     public void hover3Test(){
 
         driver.get("http://the-internet.herokuapp.com/hovers");
-        WebElement img = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[3]/img"));
+        WebElement img = driver.findElement(By.cssSelector("#content > div > div:nth-child(5) > img"));
         Actions action = new Actions(driver);
         action.moveToElement(img).perform();
 
-        WebElement user = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[3]/div/h5"));
+        WebElement user = driver.findElement(By.cssSelector("#content > div > div:nth-child(5) > div > h5"));
         String actualResult1 = user.getText();
         Assert.assertEquals(actualResult1, "name: user3");
 
-        WebElement link = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[3]/div/a"));
+        WebElement link = driver.findElement(By.linkText("View profile"));
         action.moveToElement(link).click().perform();
         WebElement name = driver.findElement(By.tagName("h1"));
         String actualResult2 = name.getText();
