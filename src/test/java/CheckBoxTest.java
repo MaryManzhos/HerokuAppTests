78045ccc7b0852964e225458af70e9c1590bb2ae
marym <maria_manzhos@tut.by>
2020-09-25 16:28:58 +0300

    remove commented code from class CheckBoxTest

--- src/test/java/CheckBoxTest.java
+++ src/test/java/CheckBoxTest.java
@@ -22,7 +22,6 @@ public class CheckBoxTest {
         options.setHeadless(false);
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        // driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
