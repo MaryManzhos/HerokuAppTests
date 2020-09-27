bc9a6c8b975559cb41a0d2aa70b8776aba01a4eb
marym <maria_manzhos@tut.by>
2020-09-27 20:19:16 +0300

    add test for checkboxes

---
+++ src/test/java/localPage/CheckBoxTest.java
@@ -0,0 +1,26 @@
+package localPage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class CheckBoxTest extends SetUp{
+    @Test
+    public void isUncheckedCheckboxes(){
+        WebElement checkBoxDogs = driver.findElement(By.id("dogs"));
+        WebElement checkBoxCats = driver.findElement(By.id("cats"));
+        assertFalse(checkBoxDogs.isSelected());
+        assertFalse(checkBoxCats.isSelected());
+    }
+
+    @Test
+    public void isCheckedCheckboxes(){
+        WebElement checkBoxDogs = driver.findElement(By.id("dogs"));
+        WebElement checkBoxCats = driver.findElement(By.id("cats"));
+        checkBoxDogs.click();
+        checkBoxCats.click();
+        assertTrue(checkBoxDogs.isSelected());
+        assertTrue(checkBoxCats.isSelected());
+    }
+}
