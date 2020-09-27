e136e6d4f69859823789452c4e3c6a0b931f848f
marym <maria_manzhos@tut.by>
2020-09-27 19:45:03 +0300

    add test for Table

---
+++ src/test/java/localPage/TableTest.java
@@ -0,0 +1,16 @@
+package localPage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class TableTest extends SetUp{
+    @Test
+    public void findElementTable(){
+        WebElement cell2 = driver.findElement(By.xpath("//tbody/tr[3]/td[2]"));
+        String actualResult = cell2.getText();
+        assertEquals(actualResult,"Petr");
+    }
+
+}
